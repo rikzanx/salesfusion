614b0905a3535abf78d7f5d9769436e58ac09122
Muhammad Rikzan <30933800+rikzanx@users.noreply.github.com>
2023-10-26 15:41:36 +0700

    save

--- database/seeders/CompanySeeder.php
+++ database/seeders/CompanySeeder.php
@@ -26,6 +26,17 @@ class CompanySeeder extends Seeder
         ]);
         config(['app.name' => "SalesFusion",]);
         config(['app.icon' => "img/img-company.svg"]);
+        config(['app.company' => Array(
+            'name' => "SalesFusion",
+            'about' => "SalesFusion is a comprehensive application that combines sales management, inventory management, as well as ERP and CRM solutions to help companies efficiently manage their operations.",
+            'address' => "123 Main Street, Anytown, USA",
+            'telp' => "+1234567890",
+            'email' => "info@salesfusion.com",
+            'image_company' => "img/img-company.svg",
+            'lat' => "40.7128",
+            'lng' => "-74.0060",
+            'saldo' => 0
+        ) ]);
         $configPath = base_path('config/app.php');
         file_put_contents($configPath, '<?php return ' . var_export(config('app'), true) . ';');
     }
