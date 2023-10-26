eeec6c92ab51b49bfa08c2507fa3886aa1351025
rikzanx <erik.kasbam@gmail.com>
2023-10-26 15:45:21 +0700

    form server

--- config/app.php
+++ config/app.php
@@ -1,6 +1,6 @@
 <?php return array (
   'name' => 'SalesFusion',
-  'icon' => 'Laravel',
+  'icon' => 'img/img-company.svg',
   'log' => 'errorlog',
   'env' => 'local',
   'debug' => true,
@@ -82,5 +82,17 @@
     'URL' => 'Illuminate\\Support\\Facades\\URL',
     'Validator' => 'Illuminate\\Support\\Facades\\Validator',
     'View' => 'Illuminate\\Support\\Facades\\View',
+  ),
+  'company' => 
+  array (
+    'name' => 'SalesFusion',
+    'about' => 'SalesFusion is a comprehensive application that combines sales management, inventory management, as well as ERP and CRM solutions to help companies efficiently manage their operations.',
+    'address' => '123 Main Street, Anytown, USA',
+    'telp' => '+1234567890',
+    'email' => 'info@salesfusion.com',
+    'image_company' => 'img/img-company.svg',
+    'lat' => '40.7128',
+    'lng' => '-74.0060',
+    'saldo' => 0,
   ),
 );
