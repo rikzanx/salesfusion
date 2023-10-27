0a368f2b9e6c0cce2445d84f44d06b0326452f06
Muhammad Rikzan <30933800+rikzanx@users.noreply.github.com>
2023-10-27 15:53:48 +0700

    fdsfd

--- resources/views/admin/new-invoice.blade.php
+++ resources/views/admin/new-invoice.blade.php
@@ -14,7 +14,7 @@
 <div id="container">
 	<div id="header">
 		<div id="logo">
-			<!-- <img src="{{ asset(config('app.company.image_company', 'img/img-company.svg')) }}" alt=""> -->
+			<img src="{{ asset(config('app.company.image_company', 'img/img-company.svg')) }}" style="width:230px;height:70px;" alt="">
 		</div>
 		<div id="reference">
 			<h3><strong>Invoice</strong></h3>
